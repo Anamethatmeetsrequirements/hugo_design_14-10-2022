03cba051ff78d95897952cdf37916a5e05da8944
Ed Sudall <edwardsudall@gmail.com>
2022-10-11 18:03:49 +0100

    18.02 commit WORKING

---
+++ oif-next-design/comps/Footer.js
@@ -0,0 +1,9 @@
+const  Footer = () => {
+    return ( 
+        <div>
+            <p>Contact | About | Cookies | Accessibility | Statistics</p>
+        </div> 
+    );
+}
+ 
+export default Footer;
